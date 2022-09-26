7bd1e721312e350ca7a267aa21f273446d535913
iswell100 <wellsikandar100@gmail.com>
2022-09-26 16:51:56 +0700

    Add Traits/ErrorResponseJson & Edit SignUpRequest

---
+++ app/Http/Requests/Auth/SignUpRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Traits\ErrorResponseJson;
+
+class SignUpRequest extends FormRequest
+{
+    use ErrorResponseJson;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:2|max:190',
+            'email' => 'required|string|email:rfc,dns|max:190',
+            'password' => 'required|string|min:6|max:190',
+        ];
+    }
+}
