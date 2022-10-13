37492ea04c27b64eee6fef07610bc202d7d62eef
iswell100 <wellsikandar100@gmail.com>
2022-10-13 16:44:39 +0700

    make show pforile

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Me\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +20,12 @@ Route::post('/sign-up', [AuthController::class, 'signUp']);
 Route::post('/sign-in', [AuthController::class, 'signIn']);
 
 Route::middleware('auth:api')->group(function() {
-    Route::get('/auth-only', [AuthController::class, function() { return response()->json(['data' => 'oy']); }]);
+
+    // Route::get('/auth-only', [AuthController::class, function() { return response()->json(['data' => 'oy']); }]);
+    Route::prefix('/me')->group(function() {
+        Route::get('/profile', [ProfileController::class, 'show']);
+    });
     Route::post('/sign-out', [AuthController::class, 'signOut']);
 });
-// tesddssss
 
 Route::middleware('jwt.refresh')->post('/refresh', [AuthController::class, 'refresh']);
